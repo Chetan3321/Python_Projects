c6a297d48e236bc9d2251b2ddfcc3d63e668be80
Chetan Khadse <chetaskhadse291@gmail.com>
2025-03-15 11:30:04 +0530

    Create Sum of number.py

---
+++ Sum of number.py
@@ -0,0 +1,9 @@
+# nums = input("Enter numbers separated by spaces: ").split()
+# nums = [int(num) for num in nums]  # Convert to integers
+# print("Sum:", sum(nums)) 
+
+numbers = [3, 7, 1, 9]
+total = 0
+for num in numbers:
+    total += num
+print("Sum:", total)
