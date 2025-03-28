c8a547fbde703f3cf902681694eb9f3b24027d51
Chetan Khadse <chetaskhadse291@gmail.com>
2025-03-28 21:48:57 +0530

    Create String2.py

---
+++ String2.py
@@ -0,0 +1,23 @@
+s1 = "me,wo,mn"
+v = s1.split(",")
+print(v)
+
+s2 = "annimal"
+v = s2.count("a")
+print(v)
+
+s3 = "Men12"
+v = s3.isalnum()
+print(v)
+
+s4 = "Men12@"
+v = s4.isalnum()
+print(v)
+
+s5 = "Womenmen"
+v = s5.isalpha()
+print(v)
+
+s6 = "Women2"
+v = s6.isalpha()
+print(v)
