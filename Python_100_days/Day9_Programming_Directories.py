56140e48999c993727c4ec55c27fd52bd57abd01
Chetan Khadse <chetaskhadse291@gmail.com>
2025-04-24 10:57:51 +0530

    updated

--- Python_100_days/Day9_Programming_Directories.py
+++ Python_100_days/Day9_Programming_Directories.py
@@ -3,4 +3,17 @@ programming_dictionary = {
 "Function": "A piece of code that you can easily call over and over again.",
 "Loop": "The action of doing something over and over again.",
 }
-print (programming_dictionary["Bug"])
+#print (programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+empty_dictionary = {}
+# Wipe an existing dictionary
+# programming dictionary = {}
+# print(programming_dictionary)
+#Edit an item in a dictionary
+programming_dictionary [ "Bug"] = "A moth in your computer."
+# print(programming_dictionary)
+# Loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
---
+++ Python_100_days/Day9_directory2.py
@@ -0,0 +1,13 @@
+capitals = {
+"France": "Paris",
+"Germany": "Berlin",
+}
+# Nested List in Dictionary
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+}
+# print Lille
+#print(travel log["France"][1])
+nested list =["A", "B", ["C", "D"]]
+print(nested list [])
