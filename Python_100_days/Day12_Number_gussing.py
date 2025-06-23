8c14cfb06328e798e91f571dabb3e6cd27d4b356
Chetan Khadse <chetaskhadse291@gmail.com>
2025-06-23 11:24:34 +0530

    Update Day12_Number_gussing.py

--- Python_100_days/Day12_Number_gussing.py
+++ Python_100_days/Day12_Number_gussing.py
@@ -3,12 +3,16 @@ from random import randint
 EASY_LEVEL_TERNS = 10
 HARD_LEVEL_TERNS = 7
 
+turns = 0
+
 # Function to check users' guess against actual answer
-def check_answer (user_guess, actual_answer):
+def check_answer (user_guess, actual_answer, turns):
     if user_guess > actual_answer:
         print("Too High.")
+        return turns - 1
     elif user_guess < actual_answer:
         print("Too Low.")
+        return turns - 1
     else:
         print(f"You got it! The answer was {actual_answer}")
         
@@ -31,13 +35,19 @@ def game():
 
 
     turns = set_difficulty()
-    print(f"You have {turns} attempts remaining to guess the number.")
 
     guess = 0
     while guess != answer:
-    # Let the user guess a number
-    guess = int(input("Make a guess: "))
-
-    check_answer(guess, answer)
+        print(f"You have {turns} attempts remaining to guess the number.")
+        # Let the user guess a number
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of the guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again.")
+            
+        
 
 game()
